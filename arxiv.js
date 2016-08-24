3dba35e3b2789c6126d1a93bd79c039e9530520f
Bryant Moscon <bmoscon@gmail.com>
2016-08-23 20:34:07 -0400

    Update source file (comments and formatting)

--- arxiv.js
+++ arxiv.js
@@ -1,51 +1,63 @@
 /* Copyright (c) 2016 Frase
  *
  * Distributed under MIT license (see LICENSE).
+ *
+ *
+ * Search arXiv via its HTTP API
+ *
+ * can search the following 1 or more fields:
+ *   - author
+ *   - title
+ *   - abstract
+ *   - journal reference
+ *   - All fields
+ *   journal's referenced, as well as all fields.
  */
- 
- /* search arXiv via its HTTP API
-  * 
-  * can search the following fields: 
-  *   - author
-  *   - title
-  *   - abstract
-  *   - journal reference
-  *   - All fields
-  *   journal's referenced, as well as all fields.
-  */
+
+
+
+/**
+ * Searches arXiv for papers/documents that match the supplied parameters
+ * @param {string} author
+ * @param {string} title
+ * @param {string} abstrct
+ * @param {string} journal_ref
+ * @param {string} all
+ * @returns {Promise}
+ */
 function arxiv_search(author, title, abstrct, journal_ref, all) {
     baseUrl = "http://export.arxiv.org/api/query?search_query=";
     first = true;
     
     if (author) {
-	    if (!first) {
-	      baseUrl += '+AND+';
-	    }
-	    baseUrl += "au:" + author;
-	    first = false;
+	if (!first) {
+	    baseUrl += '+AND+';
+	}
+	baseUrl += "au:" + author;
+	first = false;
     }
     
     if (title) {
-	    if (!first) {
-	      baseUrl += '+AND+';
-	    }
-      baseUrl += "ti:" + title;
-	    first = false;
+	if (!first) {
+	    baseUrl += '+AND+';
+	}
+	baseUrl += "ti:" + title;
+	first = false;
     }
-
+    
     if (abstrct) {
-	    if (!first) {
-	      baseUrl += '+AND+';
-	    }
-      baseUrl += "abs:" + abstrct;
-	    first = false;
+	if (!first) {
+	    baseUrl += '+AND+';
+	}
+	baseUrl += "abs:" + abstrct;
+	first = false;
     }
-
+    
     if (all) {
-	    if (!first) {
-	      baseUrl += '+AND+';
-	    }
-      baseUrl += "all:" + all;
+	if (!first) {
+	    baseUrl += '+AND+';
+	}
+	baseUrl += "all:" + all;
     }
     
     var deferred = $.Deferred();
@@ -54,29 +66,29 @@ function arxiv_search(author, title, abstrct, journal_ref, all) {
         type: "get",
         dataType: "xml",
         success: function(xml) {
-			    var entry = [];
-				  $(xml).find('entry').each(function (index) {
-			      var id = $(this).find('id').text();
-			      var pub_date = $(this).find('published').text();
-			      var title = $(this).find('title').text();
-			      var summary = $(this).find('summary').text();
-			      var authors = [];
-			      $(this).find('author').each(function (index) {
-				      authors.push($(this).text());
-			      });
-            
-			      entry.push({'title': title,
-					              'link': id,
-					              'summary': summary,
-					              'date': pub_date,
-					              'authors': authors
-				                });
-			    });
-
-			    deferred.resolve(entry);
+	    var entry = [];
+	    $(xml).find('entry').each(function (index) {
+		var id = $(this).find('id').text();
+		var pub_date = $(this).find('published').text();
+		var title = $(this).find('title').text();
+		var summary = $(this).find('summary').text();
+		var authors = [];
+		$(this).find('author').each(function (index) {
+		    authors.push($(this).text());
+		});
+		
+		entry.push({'title': title,
+			    'link': id,
+			    'summary': summary,
+			    'date': pub_date,
+			    'authors': authors
+			   });
+	    });
+	    
+	    deferred.resolve(entry);
         },
         error: function(status) {
-          console.log("request error " + status + " for url: "+baseUrl);
+            console.log("request error " + status + " for url: "+baseUrl);
         }
     });
     return deferred.promise();
